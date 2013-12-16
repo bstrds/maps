842b0777f772912730dce75087fd2ed3ac3e2979
bstrds <elapoueis@gmail.com>
2013-12-16 19:04:38 +0200

    G G G G

--- src/cs/aueb/maps/MainActivity.java
+++ src/cs/aueb/maps/MainActivity.java
@@ -32,10 +32,12 @@ import com.google.android.gms.maps.model.MarkerOptions;
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.activity_main);
 	
-			lat = new double[]{-33.867,40.714353, 52.066, 40.996484, 39.293391};
-			lon = new double[]{151.206, -74.005973, 4.292107, 28.978271, -76.587811};
-			title = new String[]{"Sydney", "New York", "The Hague", "Istanbul", "Baltimore"};
-			msg = new String[]{"filthy southern peasants", "hipsters", "V2 funland", "swagSWAGswag", "All in the game yo, all in the game."};
+			lat = new double[]{-33.867,40.714353, 52.066, 40.996484, 39.293391, 38.028622, 34.846776, 35.692995, 48.684614, -18.908885};
+			lon = new double[]{151.206, -74.005973, 4.292107, 28.978271, -76.587811, 24.000778, 24.093304, 139.756164, 44.484787, 47.526398};
+			title = new String[]{"Sydney", "New York", "The Hague", "Istanbul", "Baltimore", "Rafina", "Gaudos", "Tokyo", "Stalingrad", "Antananarivo"};
+			msg = new String[]{"Filthy southern peasants", "Hipsters", "V2 funland", "swagSWAGswag",
+					"All in the game yo, all in the game.", "Mpomp's house", "Hippies", "Hotel",
+					"Where you locate the app..", "Super fun time!"};
 			
 			
 			mMap = ((MapFragment)
